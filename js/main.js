902d8611ec45c7bef9b027c663cc39323a9b32e4
Ressy77 <reslaauma@gmail.com>
2014-02-18 16:51:13 +0300

    add event listener

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert("Hello JS");
+/*var message= false
+  if(message===undefined){
+    alert("True! " + message)
+  }
+  else{
+    alert("False! " + message)
+  }
+var button= document.getElementById('my-button')
+  console.log("button is: " + button)
+button.addEventListener('click', function(e){
+    alert("button was clicked!");
+});*/
+$('#my-button').on("click", function(){
+  alert("You clicked")})
